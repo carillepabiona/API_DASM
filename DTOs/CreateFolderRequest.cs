b1fa5cd54e7fe2785b6e32335669cf9f1f3c036b
Carille Pabiona <carillepabiona49@gmail.com>
2026-05-11 09:14:31 +0800

    USERS PAGE DONE!

---
+++ DTOs/CreateFolderRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace API_DASM.DTOs
+{
+    public class CreateFolderRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public Guid? ParentFolderId { get; set; }
+
+        public Guid CreatedBy { get; set; }
+    }
+}
